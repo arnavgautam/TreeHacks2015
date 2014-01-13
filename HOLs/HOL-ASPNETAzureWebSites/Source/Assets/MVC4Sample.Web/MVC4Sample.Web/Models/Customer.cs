88e958c82b9030f3e0d41a53e4d80d0ebb32fd89
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-13 15:19:09 -0300

    [HOL-ASPNETAzureWebSites] Added current version of the HOL to the repo

---
+++ HOLs/HOL-ASPNETAzureWebSites/Source/Assets/MVC4Sample.Web/MVC4Sample.Web/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4Sample.Web.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Company { get; set; }
+
+        public string Title { get; set; }
+
+        public string Email { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ HOLs/HOL-ASPNETAzureWebSites/Source/Assets/README.md
@@ -0,0 +1,2 @@
+﻿To run this solution you need to configure the **CustomerContext** connection string as explained in Exercise 2 - Task 2. Notice that when compiling the solution, the NuGet packages dependencies will be automatically downloaded and installed.
+
