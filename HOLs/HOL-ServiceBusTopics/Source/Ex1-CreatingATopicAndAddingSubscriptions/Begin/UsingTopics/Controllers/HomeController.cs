e321fb5b5197a464470f65bf2d24793c300efd29
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-19 20:18:53 -0300

    [HOL-ServiceBusTopics] Removed reference to Microsoft.ServiceBus assembly in Begin solution

--- HOLs/HOL-ServiceBusTopics/Source/Ex1-CreatingATopicAndAddingSubscriptions/Begin/UsingTopics/Controllers/HomeController.cs
+++ HOLs/HOL-ServiceBusTopics/Source/Ex1-CreatingATopicAndAddingSubscriptions/Begin/UsingTopics/Controllers/HomeController.cs
@@ -2,27 +2,9 @@
 {
     using System;
     using System.Web.Mvc;
-    using Microsoft.ServiceBus;
-    using Microsoft.ServiceBus.Messaging;
-    using Microsoft.WindowsAzure.ServiceRuntime;
 
     public class HomeController : Controller
     {
-        private NamespaceManager namespaceManager;
-        private MessagingFactory messagingFactory;
-
-        public HomeController()
-        {
-            var baseAddress = RoleEnvironment.GetConfigurationSettingValue("namespaceAddress");
-            var issuerName = RoleEnvironment.GetConfigurationSettingValue("issuerName");
-            var issuerKey = RoleEnvironment.GetConfigurationSettingValue("issuerKey");
-
-            Uri namespaceAddress = ServiceBusEnvironment.CreateServiceUri("sb", baseAddress, string.Empty);
-
-            this.namespaceManager = new NamespaceManager(namespaceAddress, TokenProvider.CreateSharedSecretTokenProvider(issuerName, issuerKey));
-            this.messagingFactory = MessagingFactory.Create(namespaceAddress, TokenProvider.CreateSharedSecretTokenProvider(issuerName, issuerKey));
-        }
-
         public ActionResult Index()
         {
             return this.View();
