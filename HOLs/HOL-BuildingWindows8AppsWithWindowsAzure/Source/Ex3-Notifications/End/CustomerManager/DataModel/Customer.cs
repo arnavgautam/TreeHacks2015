1cfd62c70ed1a59faf7dd3afc2880cd14d3b0d4b
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2014-01-23 16:01:14 -0300

    # Updated End solution for Ex 3 (WIP)

---
+++ HOLs/HOL-BuildingWindows8AppsWithWindowsAzure/Source/Ex3-Notifications/End/CustomerManager/DataModel/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Company { get; set; }
+
+        public string Title { get; set; }
+
+        public string Email { get; set; }
+
+        public string Image { get; set; }
+    }
+}
