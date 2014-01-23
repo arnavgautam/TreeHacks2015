19a5492999b20572381266cf7dd889ba41d7facb
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-23 14:38:37 -0300

    HOL-BuildingAppsWithCaching
    	Replaced begin solution with new project
    	Enabled Nuget Package Restore

--- HOLs/HOL-BuildingAppsWithCaching/Source/Ex1-CacheSessionState/Begin/CloudShop/Controllers/HomeController.cs
+++ HOLs/HOL-BuildingAppsWithCaching/Source/Ex1-CacheSessionState/Begin/CloudShop/Controllers/HomeController.cs
@@ -3,74 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using CloudShop.Models;
 
 namespace CloudShop.Controllers
 {
-    [HandleError]
     public class HomeController : Controller
     {
-        public ActionResult About()
-        {
-            return this.View();
-        }
-
-        public EmptyResult Recycle()
-        {
-            RoleEnvironment.RequestRecycle();
-            return new EmptyResult();
-        }
-
         public ActionResult Index()
         {
-            Services.IProductRepository productRepository = new Services.ProductsRepository();
-            var products = productRepository.GetProducts();
-
-            // add all products currently not in session
-            var itemsInSession = this.Session["Cart"] as List<string> ?? new List<string>();
-            var filteredProducts = products.Where(item => !itemsInSession.Contains(item));
-
-            IndexViewModel model = new IndexViewModel()
-            {
-                Products = filteredProducts
-            };
-
-            return View(model);
+            return View();
         }
 
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Add(string selectedItem)
+        public ActionResult About()
         {
-            if (selectedItem != null)
-            {
-                List<string> cart = this.Session["Cart"] as List<string> ?? new List<string>();
-                cart.Add(selectedItem);
-                Session["Cart"] = cart;
-            }
+            ViewBag.Message = "Your application description page.";
 
-            return RedirectToAction("Index");
+            return View();
         }
 
-        public ActionResult Checkout()
+        public ActionResult Contact()
         {
-            var itemsInSession = this.Session["Cart"] as List<string> ?? new List<string>();
-            return View(itemsInSession);
-        }
+            ViewBag.Message = "Your contact page.";
 
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Remove(string selectedItem)
-        {
-            if (selectedItem != null)
-            {
-                var itemsInSession = this.Session["Cart"] as List<string>;
-                if (itemsInSession != null)
-                {
-                    itemsInSession.Remove(selectedItem);
-                }
-            }
-
-            return RedirectToAction("Checkout");
+            return View();
         }
     }
 }
