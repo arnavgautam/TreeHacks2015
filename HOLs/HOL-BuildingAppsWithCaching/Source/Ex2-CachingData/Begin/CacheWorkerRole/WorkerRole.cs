90b24c8380fc2a386776d25f2fc4e12609f43846
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-24 16:06:26 -0300

    HOL-BuildingAppsWithCaching
    	Created Ex2 Begin solution.

--- HOLs/HOL-BuildingAppsWithCaching/Source/Ex2-CachingData/Begin/CacheWorkerRole/WorkerRole.cs
+++ HOLs/HOL-BuildingAppsWithCaching/Source/Ex2-CachingData/Begin/CacheWorkerRole/WorkerRole.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
-
 namespace CacheWorkerRole
 {
+    using Microsoft.WindowsAzure.ServiceRuntime;
+
     public class WorkerRole : RoleEntryPoint
     {
         // No further implementation is required to support a cache worker role. 
