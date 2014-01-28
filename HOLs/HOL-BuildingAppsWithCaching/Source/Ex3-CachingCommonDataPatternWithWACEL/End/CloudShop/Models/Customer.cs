c2436c83d713465cb0a9757f0cd32e6344b365e8
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2014-01-28 10:40:25 -0300

    [HOL-BuildingAppsWithCaching] Updated end solution for Exercise 3. Added begin solution.

--- HOLs/HOL-BuildingAppsWithCaching/Source/Ex3-CachingCommonDataPatternWithWACEL/End/CloudShop/Models/Customer.cs
+++ HOLs/HOL-BuildingAppsWithCaching/Source/Ex3-CachingCommonDataPatternWithWACEL/End/CloudShop/Models/Customer.cs
@@ -7,13 +7,13 @@
         public string Id { get; set; }
 
         public string Company { get; set; }
-        
+
         public string Name { get; set; }
-        
+
         public double Value { get; set; }
-        
+
         public string Comment { get; set; }
-        
+
         public DateTime ContractDate { get; set; }
     }
 }
