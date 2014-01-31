e686d4ce3dd881976658c0dee52cc14b555e3fab
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-31 16:44:10 -0300

    [HOL-DeployingCloudServices] Added en solution for Exercise 2

---
+++ HOLs/HOL-DeployingCloudServices/Source/Ex2-DeployingWithPowerShell/End/MyTodo.WebUx/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace MyTodo.WebUx.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
