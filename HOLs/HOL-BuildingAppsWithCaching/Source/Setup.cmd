e071334e224b135fe344ae651fe07be355c53cc5
mrodriguez <marcelo.rodriguez@southworks.net>
2014-01-31 12:05:21 -0300

    HOL-BuildingAppsWithCaching
    	Updated setup script to remove Dependency Checker option

--- HOLs/HOL-BuildingAppsWithCaching/Source/Setup.cmd
+++ HOLs/HOL-BuildingAppsWithCaching/Source/Setup.cmd
@@ -13,32 +13,13 @@ cls
 echo Setup Options
 echo =============
 echo.
-echo 1. Check dependencies and execute setup scripts. 
-echo    Note: requires internet connection.
+echo 1. Execute setup scripts only.
 echo.
-echo 2. Execute setup scripts only.
-echo    WARNING: The setup could fail and the Hands-on Lab might not work properly if you don't have all the prerequisites installed.
+echo 2. Exit.
 echo.
-echo 3. Exit.
-echo.
-choice /c:123 /M "Choose an option: " 
-if errorlevel 3 goto exit
-if errorlevel 2 goto execsetup
-if errorlevel 1 goto downloaddc
-
-:downloaddc
-REM Here executes the PS1 to download the DC.EXE.
-echo Downloading Dependency Checker
-
-IF EXIST %WINDIR%\SysWow64 (
-set powerShellDir=%WINDIR%\SysWow64\windowspowershell\v1.0
-) ELSE (
-set powerShellDir=%WINDIR%\system32\windowspowershell\v1.0
-)
-
-call %powerShellDir%\powershell.exe -Command Set-ExecutionPolicy unrestricted
-call %powerShellDir%\powershell.exe -Command "&'.\Setup\Scripts\RunDC.ps1'"
-goto pause
+choice /c:12 /M "Choose an option: " 
+if errorlevel 2 goto exit
+if errorlevel 1 goto execsetup
 
 :execsetup
 REM Here executes the Setup.cmd only.
