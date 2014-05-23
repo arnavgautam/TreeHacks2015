bf8b1a5b249346bde20ef1e93bf48216fd2d96df
Marcos Castany <marcos.castany@southworks.net>
2014-05-23 17:38:51 -0300

    [Demo-ClipMeme] Moved username to web.config in the appsettings

--- Demos/Demo-ClipMeme/source/code/end/ClipMeme/ClipMeme/Controllers/HomeController.cs
+++ Demos/Demo-ClipMeme/source/code/end/ClipMeme/ClipMeme/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿namespace ClipMeme.Controllers
+﻿using System.Configuration;
+
+namespace ClipMeme.Controllers
 {
     using System.Web.Mvc;
 
@@ -7,7 +9,7 @@
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            ViewBag.Username = "Marcos";
+            ViewBag.Username = ConfigurationManager.AppSettings["username"];
 
             return this.View();
         }
