cb36922b9ec8accb3f90256683a67907c7ac327d
Daniel Altamirano <daniel.altamirano@southworks.net>
2014-05-27 11:09:13 -0300

    Demo-MobileServices: Namespace for Begin/End solutions MobileService, settings class changed

--- Demos/Demo-MobileServices/Source/FacilityApp/End/Service/Settings.cs
+++ Demos/Demo-MobileServices/Source/FacilityApp/End/Service/Settings.cs
@@ -1,4 +1,4 @@
-﻿namespace MobileService.Properties {
+﻿namespace MobileService {
     // This class allows you to handle specific events on the settings class:
     //  The SettingChanging event is raised before a setting's value is changed.
     //  The PropertyChanged event is raised after a setting's value is changed.
