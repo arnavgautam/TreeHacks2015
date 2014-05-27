685285d9df14d849849392c4de273790ecbb8bfe
Marcos Castany <marcos.castany@southworks.net>
2014-05-27 18:46:25 -0300

    [ClipMeme] Created begin solution

---
+++ Demos/Demo-ClipMeme/source/code/begin/ClipMeme/ClipMeme/Hubs/GifServerHub.cs
@@ -0,0 +1,16 @@
+﻿namespace ClipMeme.Hubs
+{
+    using System;
+    using System.Diagnostics;
+    using Microsoft.AspNet.SignalR;
+
+    public class GifServerHub : Hub
+    {
+        public void GifGenerationCompleted(string clientId, string url)
+        {
+            Trace.TraceError(string.Format("GifGenerationCompleted {0} : {1}", url, DateTime.Now));
+
+            this.Clients.All.GifGenerationCompleted(url);
+        }
+    }
+}
---
+++ Demos/Demo-ClipMeme/source/code/begin/ClipMeme/ClipMeme/Scripts/app/services/signalrservice.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'signalrservice';
+
+    angular.module('app').factory(serviceId, ['$rootScope', signalrservice]);
+
+    function signalrservice($rootScope) {
+        var hubName = "GifServerHub";
+
+        var connection = $.hubConnection();
+        var proxy = connection.createHubProxy(hubName);
+        connection.error(function (error) {
+            console.log('SignalR error: ' + error);
+        });
+
+        return {
+            on: function (eventName, callback) {
+                proxy.on(eventName, function (result) {
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback(result);
+                        }
+                    });
+                });
+            },
+            off: function (eventName, callback) {
+                proxy.off(eventName, function (result) {
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback(result);
+                        }
+                    });
+                });
+            },
+            invoke: function (methodName, callback) {
+                proxy.invoke(methodName)
+                    .done(function (result) {
+                        $rootScope.$apply(function () {
+                            if (callback) {
+                                callback(result);
+                            }
+                        });
+                    });
+            },
+            init: function() {
+                return connection.start();
+            },
+            connection: connection
+        };
+    }
+})();
