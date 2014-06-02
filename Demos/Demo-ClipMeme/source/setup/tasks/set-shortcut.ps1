edceca585e773ffc1f55e82d86ddf02e8c2ad68b
Hernan Meydac Jean <hernan.meydac@southworks.net>
2014-06-02 17:36:29 -0300

    [ClipMeme] Moved scripts to Setup folder. Removed unused scripts and files

--- Demos/Demo-ClipMeme/source/setup/tasks/set-shortcut.ps1
+++
@@ -1,6 +0,0 @@
-param ( [string]$SourceExe, [string]$ArgumentsToSourceExe, [string]$DestinationPath )
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($DestinationPath)
-$Shortcut.TargetPath = $SourceExe
-$Shortcut.Arguments = $ArgumentsToSourceExe
-$Shortcut.Save()
