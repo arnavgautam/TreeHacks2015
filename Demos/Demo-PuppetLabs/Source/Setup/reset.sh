7fad7365c596c45be3237e6825e826560bb9774b
Hernan Meydac Jean <hernan.meydac@southworks.net>
2014-06-13 15:54:40 -0300

    [Puppet] Updated script to configure Puppet Groups and class

--- Demos/Demo-PuppetLabs/Source/Setup/reset.sh
+++ Demos/Demo-PuppetLabs/Source/Setup/reset.sh
@@ -5,13 +5,19 @@ host=adminuser@hmeydacmaster.cloudapp.net
 pwd
 ssh-keygen -f "~/.ssh/known_hosts" -R $host
 
-#scp -r /assets/microsoft-sysinternals adminuser@hmeydacmaster.cloudapp.net:/tmp
-
 ssh $host 'bash -s' << EOF
+	# Create admin user
 	cd /opt/puppet/share/puppet-dashboard
 	sudo /opt/puppet/bin/bundle exec /opt/puppet/bin/rake -f /opt/puppet/share/console-auth/Rakefile db:create_user USERNAME=$username PASSWORD=$password ROLE='Admin'
+	# Install Puppet prerequisites
 	/opt/puppet/bin/puppet module install puppetlabs-registry
-	/opt/puppet/bin/puppet module install joshcooper-powershell                               
+	/opt/puppet/bin/puppet module install joshcooper-powershell      
+
+	# Create Group if not exists
+	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:add['Windows Servers']
+
+	# Remove classes from Group
+	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:delclass['Windows Servers','microsoft-sysinternals']	
 EOF
 
 read -p 'Press [Enter] key to exit...'
