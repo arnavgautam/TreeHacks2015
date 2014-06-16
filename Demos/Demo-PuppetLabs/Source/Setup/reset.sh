6a1aaf856ab58e2c151956f6d1db7182596a48a5
Hernan Meydac Jean <hernan.meydac@southworks.net>
2014-06-16 11:42:44 -0300

    [Puppet] Added bash reset script

--- Demos/Demo-PuppetLabs/Source/Setup/reset.sh
+++ Demos/Demo-PuppetLabs/Source/Setup/reset.sh
@@ -1,23 +1,22 @@
-#read -p 'Puppet Master VM Username (e.g.: admin@vmname.cloudapp.net): ' host
-username=admin@contoso.com
-password=Passw0rd!
 host=adminuser@hmeydacmaster.cloudapp.net
-pwd
-ssh-keygen -f "~/.ssh/known_hosts" -R $host
+username=$2
+password=$3
 
-ssh $host 'bash -s' << EOF
-	# Create admin user
-	cd /opt/puppet/share/puppet-dashboard
-	sudo /opt/puppet/bin/bundle exec /opt/puppet/bin/rake -f /opt/puppet/share/console-auth/Rakefile db:create_user USERNAME=$username PASSWORD=$password ROLE='Admin'
-	# Install Puppet prerequisites
-	/opt/puppet/bin/puppet module install puppetlabs-registry
-	/opt/puppet/bin/puppet module install joshcooper-powershell      
+scp -r ./Assets/microsoft-sysinternals $host:/tmp
 
-	# Create Group if not exists
-	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:add['Windows Servers']
-
-	# Remove classes from Group
-	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:delclass['Windows Servers','microsoft-sysinternals']	
-EOF
+#ssh $host 'bash -s' << EOF
+#	# Create admin user
+#	cd /opt/puppet/share/puppet-dashboard
+#	sudo /opt/puppet/bin/bundle exec /opt/puppet/bin/rake -f /opt/puppet/share/console-auth/Rakefile db:create_user USERNAME=$username PASSWORD=$password ROLE='Admin'
+#	# Install Puppet prerequisites
+#	/opt/puppet/bin/puppet module install puppetlabs-registry
+#	/opt/puppet/bin/puppet module install joshcooper-powershell      
+#
+#	# Create Group if not exists
+#	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:add['Windows Servers']
+#
+#	# Remove classes from Group
+#	sudo /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodegroup:delclass['Windows Servers','microsoft-sysinternals']	
+#EOF
 
 read -p 'Press [Enter] key to exit...'
